237c6c6341fc36af623ed8b5969e88027b90c747
Marcelo Rodrigues Campos <44681621+wwwxkz@users.noreply.github.com>
2022-11-28 11:01:35 -0300

    Fixed missing guest-ticket shortcode component

--- shortcode/guest.php
+++ shortcode/guest.php
@@ -4,11 +4,16 @@ session_start();
 
 // Components
 include_once('components/guest.php');
+include_once('components/guest-ticket.php');
 include_once('components/styles/main.php');
 
 function wdesk_shortcode_guest() {
     $return = '';
-	$return .= wdesk_shortcode_component_guest();
+	if (isset($_GET['ticket']) && isset($_GET['token'])) {
+		$return .= wdesk_shortcode_component_guest_ticket();
+	} else {
+		$return .= wdesk_shortcode_component_guest();
+	}
     $return .= wdesk_shortcode_component_style_main();
     return $return;
 }
